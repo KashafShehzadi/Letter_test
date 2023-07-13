5ebb35f3fde1a18a236c5c096d0412db833e8559
user <kashafshehzadi480@gmail.com>
2023-07-13 11:40:43 +0500

    3rd Final Commit

--- app/src/main/java/com/example/letters_test/HomeFragment.java
+++ app/src/main/java/com/example/letters_test/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.letters_test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    private Button githubButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -48,6 +52,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -58,7 +63,24 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        githubButton = rootView.findViewById(R.id.githubButton);
+        githubButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGitHubProject();
+            }
+        });
+
+        return rootView;
+    }
+    private void openGitHubProject() {
+        // Replace "https://github.com/your-project-url" with your GitHub project URL
+        String githubProjectUrl = "https://github.com/KashafShehzadi/Letter_test";
+
+        // Open the GitHub project URL in a web browser
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubProjectUrl));
+        startActivity(intent);
     }
 }
